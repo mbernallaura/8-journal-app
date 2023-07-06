b4ed9a1155897fcc8b387ed6e697fa36a99a1244
mbernallaura <mbernallaura@gmail.com>
2023-07-06 18:28:27 -0500

    Realizar la visualizacion del navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material';
+import { Navbar } from '../components/Navbar';
+
+const drawerWidth= 240; 
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{display: 'flex'}}>
+            <Navbar drawerWidth={ drawerWidth }/>
+
+            {/* Sidebar */}
+            <Box 
+                component='main'
+                sx={{ flexGrow:1, p:3 }}
+            >
+                {/* Toolbar */}
+
+                { children }
+            </Box>
+        </Box>
+    )
+}
