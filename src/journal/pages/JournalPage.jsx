c9eb21995f4912ae318fa22f1f30d4004860f2d4
mbernallaura <mbernallaura@gmail.com>
2023-07-06 19:56:15 -0500

    Desarrollar la view de Journal app

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>JournalPage ajhhhhhhhshshshssjsjsjsjssjjsjsjsjssj</Typography>
-      {/* NothingSelected
-      NoteView */}
+      {/* <Typography>JournalPage ajhhhhhhhshshshssjsjsjsjssjjsjsjsjssj</Typography> */}
+      <NothingSelectedView/>
+      {/* NoteView  */}
     </JournalLayout>
   )
 }
