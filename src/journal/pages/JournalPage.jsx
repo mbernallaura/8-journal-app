0c2dbe0ddd12c96275c3ab016b57361f9dac7d8b
mbernallaura <mbernallaura@gmail.com>
2023-07-07 18:32:34 -0500

    Agregar el boton de mas para poder agregar una nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -6,8 +8,22 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>JournalPage ajhhhhhhhshshshssjsjsjsjssjjsjsjsjssj</Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+        size='large'
+        sx={{ 
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
