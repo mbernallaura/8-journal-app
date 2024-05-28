b47ee1a9d1d5a0d3e3ef6245787e9d72b9d5bbe4
laura <mbernallaura@gmail.com>
2024-05-27 19:20:22 -0500

    esquema de redux para loguearse en la aplicacion hacer los archivos barrill instalar las herramientas de redux y redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -8,8 +8,8 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>JournalPage ajhhhhhhhshshshssjsjsjsjssjjsjsjsjssj</Typography> */}
-      <NothingSelectedView/>
-      {/* <NoteView/> */}
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
 
       <IconButton
         size='large'
