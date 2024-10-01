69ee693335a81190f3a856bc5e0ffe128f882a92
laura <mbernallaura@gmail.com>
2024-09-30 21:47:26 -0500

    visualizar las notas en la app y mejorar la estrutura de codigo

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,13 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./SideBarItem";
 
 
 export const SideBar = ({ drawerWidth= 240 }) => {
     const { displayName } = useSelector(state => state.auth);
+    const { notes } = useSelector(state => state.journal);
+    
     return (
         <Box
             component='nav'
@@ -28,18 +31,8 @@ export const SideBar = ({ drawerWidth= 240 }) => {
 
                 <List>
                     {
-                        ['Enero', 'Febrero', 'Marzo'].map(text =>(
-                            <ListItem key={ text } disablePadding > 
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot/>
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={ text }/>
-                                        <ListItemText secondary={ 'Hajadkddd jakjsjdkbds asdjhgs' }/>
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map(note =>(
+                            <SideBarItem key={ note.id } { ...note }/>
                         ))
                     }
                 </List>
