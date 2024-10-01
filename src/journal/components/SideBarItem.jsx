1e4e3826e1acbdd4ddf506e09ec4013e3cd53a33
laura <mbernallaura@gmail.com>
2024-09-30 22:05:54 -0500

    Se guarda en el redux activeNote la nota que se selecciona en la app

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,14 +1,22 @@
 import { TurnedInNot } from '@mui/icons-material'
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React, { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ title, id, body }) => {
+export const SideBarItem = ({ title, id, body, date, imageUrls= [] }) => {
+    const dispatch = useDispatch();
     const newTitle = useMemo(() =>{
         return title.length > 17 ? title.substring(0, 17) + '...' : title;
-    }, [title])
+    }, [title]);
+
+    const handleActiveNote = ()=>{
+        dispatch( setActiveNote({ title, id, body, date, imageUrls }) );
+    }
+
     return (
-        <ListItem key={ id } disablePadding > 
-            <ListItemButton>
+        <ListItem key={ id } disablePadding> 
+            <ListItemButton onClick={ handleActiveNote }>
                 <ListItemIcon>
                     <TurnedInNot/>
                 </ListItemIcon>
